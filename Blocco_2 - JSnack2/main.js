76c57ec9e16504c3a544ad620698349568562c97
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-11 17:04:57 +0100

    feat: add const block 2 - jsnack 2

---
+++ Blocco_2 - JSnack2/main.js
@@ -0,0 +1,5 @@
+//Costante per creare una lista di nomi
+const nomi= ["Mike", "Dustin", "Lucas", "Will", "Jim", "Max", "Steve", "Jane"]
+
+//Costante per creare una lista di cognomi
+const cognomi= ["Wheeler", "Henderson", "Sinclair", "Byers", "Hopper", "Mayfield", "Harrington", "Ives"]
