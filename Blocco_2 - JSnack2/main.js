4859ffa983704a46c72ee967d629d40c4d696edf
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-11 17:22:54 +0100

    feat: add function main.js block-2 JSnack2

--- Blocco_2 - JSnack2/main.js
+++ Blocco_2 - JSnack2/main.js
@@ -2,4 +2,19 @@
 const nomi= ["Mike", "Dustin", "Lucas", "Will", "Jim", "Max", "Steve", "Jane"]
 
 //Costante per creare una lista di cognomi
-const cognomi= ["Wheeler", "Henderson", "Sinclair", "Byers", "Hopper", "Mayfield", "Harrington", "Ives"]
+const cognomi= ["Wheeler", "Henderson", "Sinclair", "Byers", "Hopper", "Mayfield", "Harrington", "Ives", "Shelby", "Stinson", "Bing"]
+
+//Costante per generare il numero degli invitati
+let numeroPartecipanti= 10;
+
+//Funzione per generare la falsa lista
+function generaNomeCognomeCasuale (){
+    let nomeCasuale = nomi[Math.floor(Math.random() * nomi.length)]; 
+    let cognomeCasuale = cognomi[Math.floor(Math.random()*nomi.length)];
+    return nomeCasuale + " " + cognomeCasuale;
+}
+
+//Creazione della lista dei falsi invitati
+for (let i = 0; i <  numeroPartecipanti; i++) {
+    console.log(generaNomeCognomeCasuale());
+}
